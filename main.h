b200f15d81460134f50636fe020b42825a3189ff
joeyhanni2 <joeyhanni@gmail.com>
2024-03-09 06:18:30 -0800

    _isdigit.c

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+
+#endif
